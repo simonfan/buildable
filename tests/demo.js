4c56305ba4c41dc8b835458a123e7e66a768fd52
simonfan <simonxd@gmail.com>
2013-06-07 16:30:29 -0300

    modified demo, now it creates two different buildable objects and displays that their behaviours are independant

--- tests/demo.js
+++ tests/demo.js
@@ -3,13 +3,20 @@ define(['buildable'], function(Buildable) {
 
 	// extend the buildable
 	Demo = Demo.extend({
-		init: function(data) {
-			console.log(data);
+		init: function(data1, data2) {
+			this.data1 = data1;
+			this.data2 = data2;
+
+			console.log('data1: ', data1);
+
+			console.log('data2: ', data2);
 		}
 	});
 
 	// initialize a new object
-	window.demo = Demo.build({ a: 1 });
+	window.demo1 = Demo.build('demo1', { a: 1 });
+
+	window.demo2 = Demo.build('demo2', { b: 9 });
 
 	console.log('Component module demo running');
 });
